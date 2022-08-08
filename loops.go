0815ab27654d9aca6ff9c76626458e46ffedc961
KUNJ <patelkunj2021@gmail.com>
2022-08-08 15:51:53 +0530

    this are some loop control of golang

---
+++ loops.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+ 
+ for i:=0;i<10;i++{
+	fmt.Println("the value of this number is just",i)
+
+ }
+
+// var a="hello world"
+// var age=23  
+	// fmt.Println(kunj)
+}
