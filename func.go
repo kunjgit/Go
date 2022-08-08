a80377d036a49b08e117993bf534cd96cff11159
KUNJ <patelkunj2021@gmail.com>
2022-08-08 16:09:25 +0530

    i have added some sort of function syntax in the go lang

---
+++ func.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func fact(n int) int{
+	if(n==0){
+		return 1
+	}
+
+	return n*fact(n-1)
+
+}
+
+func main (){
+	var a int
+	a=4
+	var ans=fact(a);
+
+	fmt.Printf("the factorial of number %d is %d ", a,ans)
+
+}
